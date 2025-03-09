44846407fc05e77dc9128004c490540495758a37
Androz2091 <androz2091@gmail.com>
2025-03-09 14:32:51 +0100

    feat: add channel ids to config service

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,17 @@
 import { Config, Effect, Layer } from 'effect';
 
 const make = Effect.gen(function* () {
-  const port = yield* Config.number('DISCORD_TOKEN');
+  const discordToken = yield* Config.string('DISCORD_TOKEN');
   const zapierWebhookUrl = yield* Config.string('WEBHOOK_URL');
+  const channelIds = yield* Config.string('CHANNEL_IDS').pipe(
+    Config.withDefault(''),
+    Config.map((ids) => ids.split(','))
+  );
 
   return {
-    port,
-    zapierWebhookUrl
+    discordToken,
+    zapierWebhookUrl,
+    channelIds
   } as const;
 });
 
